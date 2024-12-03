c67befc703d4ed7ceba0f5da4b8e7b4af91672a6
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 12:30:41 +0100

    Connection has been changed

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if os.environ.get('FLASK_ENV') == 'development':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('EXTERNAL_DATABASE_URL')
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('INTERNAL_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
