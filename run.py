a48cb1bc28da800a0d7db4961f05b736aee35b20
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 13:01:53 +0100

    run.py has been changed

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from flask_migrate import upgrade
+import os
 
 if __name__ == '__main__':
+    with app.app_context():
+        if not os.path.exists('app.db'):
+            db.create_all()
+        upgrade()
     app.run(debug=True)
