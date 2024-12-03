98114042a4f30396cd6465a5d3d45d4b02e58d82
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 12:14:03 +0100

    Connection has been changed

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://card_container_base_user:q6z7i42DbOJRLxaUdhnonToqpd0UAL1v@dpg-ct7d5a52ng1s73cdebm0-a.frankfurt-postgres.render.com/card_container_base')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
