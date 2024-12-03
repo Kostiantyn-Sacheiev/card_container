2fe5f7e056b33bb7a7b118f022bf5d0167866a35
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 11:02:45 +0100

    Some files to deploy were been changed

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://card_container_base_user:q6z7i42DbOJRLxaUdhnonToqpd0UAL1v@dpg-ct7d5a52ng1s73cdebm0-a/card_container_base') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
