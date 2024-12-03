343127448adaee9845c5421152641f625200b3de
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 14:28:17 +0100

    CONFIG!!!

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
 
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL') or
+        os.environ.get('EXTERNAL_DATABASE_URL') or
+        os.environ.get('INTERNAL_DATABASE_URL') or
         'sqlite:///' + os.path.join(basedir, 'app.db')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
