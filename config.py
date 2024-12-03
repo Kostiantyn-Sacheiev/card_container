f4b7c2031bf8993c71d697d633508ee520822db6
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2024-12-03 13:14:45 +0100

    config.py has been changed

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        os.environ.get('EXTERNAL_DATABASE_URL') or \
-        os.environ.get('INTERNAL_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    if os.environ.get('FLASK_ENV') == 'development':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('EXTERNAL_DATABASE_URL')
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('INTERNAL_DATABASE_URL')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
